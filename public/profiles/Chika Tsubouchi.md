2bf210357e848e4cd1561b8d2e932a0b8024a8e1
yutadd <spyk4m49@gmail.com>
2023-03-10 18:27:20 +0900

    update speaker profile

--- public/profiles/Chika Tsubouchi.md
+++
@@ -1,8 +0,0 @@
-# h1
-## h2
-### h3
-hello  
-[リンク](http://www.google.com/)
-![画像](https://bayashita.com/img/sanma.gif)  
-*aaa*
-
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,13 +4,38 @@ import { unified } from 'unified';
 import ReactMarkdown from 'react-markdown';
 // Support GFM (tables, autolinks, tasklists, strikethrough)
 import remarkGfm from 'remark-gfm';
-
+import rehypeRaw from "rehype-raw";
+import rehypeSanitize from "rehype-sanitize";
 import { Outter } from '../Outter/Outter';
 import "./Profile.css";
+import YouTube from 'react-youtube';
+let lang: any;
+function prepare() {
+    var userLang = navigator.language;
+    if (userLang.trim() === "ja") {
+        lang = require("../../locales/speakers/ja.json");
+    } else {
+        lang = require("../../locales/speakers/en.json");
+    }
+}
+function youtubePanel(name: string) {
+    prepare();
+    const elms: JSX.Element[] = [];
+    for (const elm of lang["speakers"]) {
+        if (elm["name"] == name) {
+            for (const vids of elm["youtube"]) {
+                elms.push(<div key={vids} className="youtube-outter"><YouTube videoId={vids}></YouTube></div>)
+            }
+            return (elms);
+        }
+    }
+    console.log("name not found");
+    return (elms);
+
+}
 export const Profile = (props: any) => {
-    
+
     const [text, setText] = useState("");
-    const [analyzed, setAnalyzed] = useState("")
     let name = props.match.params.name;
     fetch("/profiles/" + name + ".md").then((res) => res.text().then((tx) => {
         setText(tx);
@@ -18,15 +43,29 @@ export const Profile = (props: any) => {
     return (
         <>
             <Outter>
-                <div className='contexts-outter'>
-                    {text ? (
-                        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                            {text}
-                        </ReactMarkdown>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
+                <div className='page_title_outter'>
+                    <h1 className='profile-title'>
+                        LiveSpeakers
+                    </h1>
+                    <h4 className='profile-subtitle'>
+                        Speakers on TEDxFukuoka
+                    </h4>
                 </div>
+                <div className='profile-outter'>
+                    <div className='contexts-outter'>
+                        {text ? (
+                            <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+                                {text}
+                            </ReactMarkdown>
+                        ) : (
+                            <p>Loading...</p>
+                        )}
+                    </div>
+                    <div className='embedding-outter'>
+                        {youtubePanel(name)}
+                    </div>
+                </div>
+
             </Outter>
 
         </>
