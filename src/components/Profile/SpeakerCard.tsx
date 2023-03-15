4137c4514bb68abee102820e2fe20bc71ba0403c
yutadd <spyk4m49@gmail.com>
2023-03-15 12:14:44 +0900

    update speaker link

--- src/components/Profile/SpeakerCard.tsx
+++ src/components/Profile/SpeakerCard.tsx
@@ -1,7 +1,7 @@
 import "./SpeakerCard.css";
 export const SpeakerCard = (props: any) => {
     return (<>
-        <div key={props.file} className="card-container">
+        <div key={props.file} id={props.file} className="card-container">
             <img src={props.file} alt={props.file} className="card-image" />
             <div className="card-text-outter">
                 <div className="card-name">{props.name}</div>
--- src/components/home/SpeakerTile.tsx
+++ src/components/home/SpeakerTile.tsx
@@ -3,7 +3,7 @@ export const SpeakerTile = (props: any): JSX.Element => {
     return (
         <>
             <div className="box">
-                <a href="/profiles#SPEAKERA" ><img className="image" width="100%" height="auto" src={props.file == "" ? "images/unknown.png" : "images/" + props.file} alt="" >
+                <a href={"/profiles#" + props.file} ><img className="image" width="100%" height="auto" src={props.file == "" ? "images/unknown.png" : "images/" + props.file} alt="" >
                 </img></a>
                 <a href="" className="category">{props.category}</a>
                 <div className="title">
