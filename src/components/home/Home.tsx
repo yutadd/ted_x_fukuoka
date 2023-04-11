e4beb2212fcb13fe291d291037bd5e84325d8f1f
yutadd <spyk4m49@gmail.com>
2023-04-11 18:13:25 +0900

    [update]del old partners

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -16,16 +16,23 @@ import { stateContext } from "../../App";
 export const Home = (): JSX.Element => {
     //表示のモードを取得・設定できる共有された変数の取得。
     const context = useContext(stateContext);
-
+    const [speakers, setspeakers] = useState([]);
     //登壇者タイルに表示される言語情報の読み込み
     const lang = context.speakerLang;
     //登壇者パネルの準備
-    let speakers = [];
-    for (const elm of lang["speakers"]) {
-        if (elm["category"] === context.category) {
-            speakers.push(<SpeakerTile key={elm["name"]} name={elm["name"]} category={elm["category"]} file={elm["file"]} />);
-        }
-    }
+    useEffect(() => {
+        setspeakers(lang["speakers"]
+            .filter((speaker: any) => speaker.category === context.category)
+            .map((speaker: any) => (
+                <SpeakerTile
+                    key={speaker.name}
+                    name={speaker.name}
+                    category={speaker.category}
+                    file={speaker.file}
+                />
+            )));
+    }, []);
+    // Use map instead of for loop
     //読み込み終了後200ミリ秒後にURLのハッシュがある位置にスクロールする
     useEffect(() => {
         setTimeout(() => {
@@ -34,11 +41,11 @@ export const Home = (): JSX.Element => {
             console.log("scrolling to " + targetEl);
             targetEl?.scrollIntoView({ behavior: 'smooth' });
         }, 200);
-    }, []);
+    }, [window.location.hash]);
     return (
         <Outter>
             <div className="top" >
-                <a href={(context.category == "2023" ? "/event/tedxfukuoka2023" : "/event/tedxfukuoka2020")} className="top-inner">
+                <a href={(context.category == "2023" ? "http://bit.ly/3Jfx83D" : "/event/tedxfukuoka2020")} className="top-inner">
                     <img className="background-image" src={"/images/" + (context.category == "2023" ? "concept_image.jpg" : "concept_image2020.jpg")} alt="" />
                 </a>
             </div>
